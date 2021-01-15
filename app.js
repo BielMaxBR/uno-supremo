97fb57c0613eec0349a5e6890dcd63f221249129
Gabriel Maximo <gbielmaximo@gmail.com>
2021-01-15 22:17:05 +0000

    arrumei pra comecar sério

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const bodyParser = require('body-parser');
-const socketIo = require('socket.io')
-const express = require('express')
-const path = require('path')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
-const io = socketIo(server)
-const WebSocket = require('ws');
+const bodyParser = require('body-parser')
+    ,socketIo = require('socket.io')
+    ,express = require('express')
+    ,path = require('path')
+    ,http = require('http')
+    ,app = express()
+    ,server = http.createServer(app)
+    ,io = socketIo(server)
+    ,WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
-
-const socketController = require('./socketController.js')
+const wss = new WebSocket.Server({ port: 8080 })
+    ,socketController = require('./socketController.js');
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'front')))
@@ -18,5 +17,8 @@ app.use(express.static(path.join(__dirname, 'front')))
 io.sockets.on('connection', socketController)
 wss.on('connection', function connection(ws) {
     console.log('Weebsocket conectado')
+    ws.on('ping',()=>{
+        console.log('Websocket ouvido')
+    }) 
 })
 module.exports = server
--- front/index.js
+++ front/index.js
@@ -1,49 +1,23 @@
-var canvas = document.getElementById('view')
-var ctx = canvas.getContext('2d')
-var img = image = document.getElementById('source');
-var pos = 0
+try {
+    const ws = new WebSocket('wss://8080-e0e5ada3-0298-4380-8010-5074091bf40e.ws-us03.gitpod.io/');
+    ws.onopen = function () {
+        ws.send('ping')
+    };
+}
+catch (err) {
+    console.log(err)
+}
+
+const canvas = document.getElementById('view')
+    ,ctx = canvas.getContext('2d')
+    ,img = image = document.getElementById('source');
+
 function draw() {
     ctx.clearRect(0,0,1000,1000)
-    // ctx.drawImage(img, pos, 0, 10, 10)
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(pos,0,10,10)
-    pos+=10
-    console.log('da')
-    setTimeout(draw,1000)
+    ctx.drawImage(img, 0, 0, 256, 256)
 }
 
-// draw()
+setTimeout(draw,100)
 
-const ws = new WebSocket('wss://8080-e0e5ada3-0298-4380-8010-5074091bf40e.ws-us03.gitpod.io/');
 var server = io.connect(location.href);
 
-ws.onopen = function () {
-    ws.send('ping')
-};
-let click = false; 
-document.addEventListener( 
-    'mouseup', () => click = false); 
-
-canvas.addEventListener('mousedown',() => {
-    click = true
-});
-document.addEventListener('mousemove', (event) => {
-    if (click) {
-        console.log(event);
-        var mousepos = getMousePos(canvas,event)
-        ctx.fillStyle = "#ffffff"
-        ctx.fillRect(mousepos.x,mousepos.y,2,2)
-    }
-}); 
-
-
-function  getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect(), // abs. size of element
-      scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
-      scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
-
-  return {
-    x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-    y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
-  }
-}
