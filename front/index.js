eea67e337018204a261af0798223e39cbe705177
Gabriel Maximo <gbielmaximo@gmail.com>
2021-01-16 22:09:58 +0000

    frontend andando

--- front/index.js
+++ front/index.js
@@ -9,14 +9,51 @@ function draw() {
 
 setTimeout(draw,100)
 
-var server = io.connect(location.href);
+var io = io.connect(location.href);
 var connected = false
-server.on('connect',()=>{
+
+var myName = ''
+var roomsOn = []
+io.on('connect',()=>{
     connected = true
+})
+
+io.on('updateRooms', (rooms) =>{
+    console.log(rooms)
+    var salas = document.getElementById('rooms')
+    roomsOn = rooms
+    salas.innerHTML = ''
+    for ( sala in roomsOn ) {
+        salas.innerHTML += '<li>'+roomsOn[sala]
+            +"<button style=\"\" onclick=\"connect(myName.toString(),\'"
+            +roomsOn[sala]+"\')\">Entrar</button>"+'</li>'
+    }
 })
 
 function createRoom(room) {
     if (connected) {
-        server.emit('createRoom',room)
+        io.emit('createRoom',room)
     }
 }
+
+function connect(name, sala) {
+    if (name) {
+        connected = true
+        socket.emit('addUser', name, sala)
+    }
+    else {
+        console.log('insira um nome')
+    }
+}
+
+document.getElementById('myname').addEventListener('keyup', function(){
+    myName = this.value
+});
+
+document.getElementById('myroom').addEventListener('keyup', function(e){
+    var key = e.code;
+    if (key == "Enter") {
+        createRoom(this.value)
+        this.value = ""
+    }
+});
